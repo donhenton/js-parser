bd1d19bb45e6f103efc8ec6cbb0f4730848a8e15
Don Henton <donhenton@gmail.com>
2017-03-18 16:31:20 -0500

    error handling

--- src/code/index.js
+++ src/code/index.js
@@ -1,17 +1,24 @@
 import TreeParser from './parser/treeParser'
- 
- function doParse()
- {
-     
-     let input = $('#inputValue').val();
-     input = input.toUpperCase();
-     let parser = new TreeParser(input);
-     parser.parse();
-     $("#logicString").html(parser.toString());
-     let treeText = JSON.stringify(parser.getTree(),null,2);
-     $('#treeResults').val(treeText);
-      
-     
- }
- 
- $('#parseButton').bind('click',doParse);
+
+        function doParse()
+        {
+
+            let input = $('#inputValue').val();
+            input = input.toUpperCase();
+            let parser = new TreeParser(input);
+            try
+            {
+                parser.parse();
+                $("#logicString").html(parser.toString());
+                let treeText = JSON.stringify(parser.getTree(), null, 2);
+                $('#treeResults').val(treeText);
+            } catch (e)
+            {
+                $("#logicString").html("<b>Error:</b> "+e.message);
+            }
+
+
+
+        }
+
+$('#parseButton').bind('click', doParse);
