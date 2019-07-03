0571083aece8e9f7595a2a903157a04d2810587e
Don Henton <donhenton@gmail.com>
2019-07-03 13:50:22 -0500

    error tests

--- test/cucumber/features/parse.feature
+++ test/cucumber/features/parse.feature
@@ -9,3 +9,26 @@ Then The result should be equal
 Scenario: Tree Parser should handle single element with parans
 Given The treeParser is loaded with A in parentheses
 Then the result should also be equal
+
+Scenario Outline: The Tree Parser should process the following correctly
+    Given input is "<input>" and the parser is set up
+    Then it should yield a result of  "<result>"
+
+  Examples:
+    | input          |result |
+    | A OR B OR C    | {\"op\":\"OR\",\"left\":{\"op\":\"OR\",\"left\":{\"property\":\"A\"},\"right\":{\"property\":\"B\"}},\"right\":{\"property\":\"C\"}}  |
+    | (A OR B) AND C | {\"op\":\"AND\",\"left\":{\"op\":\"OR\",\"left\":{\"property\":\"A\"},\"right\":{\"property\":\"B\"}},\"right\":{\"property\":\"C\"}} |
+    | 45 AND 37      | {\"op\":\"AND\",\"left\":{\"property\":\"45\"},\"right\":{\"property\":\"37\"}} |
+
+
+Scenario Outline: The Tree Parser should be able to handle error conditions
+    Given erroneous input of "<error_input>" and the parser is set up
+    Then an error should be thrown with message of "<error_message>"
+
+  Examples:
+    |error_input|error_message|
+    |45 snort 37 | something went wrong |
+    |AND|invalid node passed|
+    |NOT|invalid node passed|
+    |A & B  | unexpected token '&' | 
+
--- test/cucumber/steps/parse_steps.js
+++ test/cucumber/steps/parse_steps.js
@@ -3,38 +3,72 @@ const expect = require('chai').expect;
 import TreeParser from './../../../src/code/parser/treeParser';
 import {Given, When, Then} from 'cucumber';
 
- 
 
-  
-  let treeParser = null;
+
+
+let treeParser = null;
+let currentError = null;
 
 //Given The parse is loaded with 'A'
 //Then The result should be equal to <''{"property":"A"}''>
+let sampleString = null;
 
-  Given('The treeParser is loaded with "A"', function () {
-    treeParser = new TreeParser('A');
-    treeParser.parse();
-  })
-  
-  
-  Then('The parser should be not null', function () {
-     
-     expect(treeParser).to.not.equal(null)
-  });
+Given('The treeParser is loaded with {string}', function (input) {
+  treeParser = new TreeParser(input);
+  treeParser.parse();
+})
 
-  Then('The result should be equal', function () {
- 
-     expect('{"property":"A"}').equal(JSON.stringify(treeParser.getTree()));
-  });
+
+Then('The parser should be not null', function () {
+
+  expect(treeParser).to.not.equal(null)
+});
+
+Then('The result should be equal', function () {
+
+  expect('{"property":"A"}').equal(JSON.stringify(treeParser.getTree()));
+});
 
 //Given The treeParser is loaded with "(A)"
 //Then the result should also be equal
- 
- Given('The treeParser is loaded with A in parentheses', function () {
-    treeParser = new TreeParser('(A)');
+
+Given('The treeParser is loaded with A in parentheses', function () {
+  treeParser = new TreeParser('(A)');
+  treeParser.parse();
+})
+Then('the result should also be equal', function () {
+
+  expect('{"property":"A"}').equal(JSON.stringify(treeParser.getTree()));
+});
+
+
+
+Given('input is {string} and the parser is set up', function (input) {
+  treeParser = new TreeParser(input);
+  treeParser.parse();
+});
+
+
+Then('it should yield a result of  {string}', function (testItem) {
+
+  expect(testItem).equal(JSON.stringify(treeParser.getTree()));
+});
+
+Given('erroneous input of {string} and the parser is set up', function (input) {
+  treeParser = new TreeParser(input);
+  try {
     treeParser.parse();
-  })
- Then('the result should also be equal', function () {
+  } catch(e) {
+    currentError = e;
+
+  }
+
+});
+
+Then('an error should be thrown with message of {string}', function (error) {
+  // Write code here that turns the phrase above into concrete actions
+   expect(currentError.message).equal(error);
+});
+
+
  
-     expect('{"property":"A"}').equal(JSON.stringify(treeParser.getTree()));
-  });
