f23e57d3578c0d1ad56e0c1d00277bf89784539f
Don Henton <donhenton@gmail.com>
2019-10-09 13:42:41 -0500

    working on reporting 5

--- test-docker/conf/jasmine.conf.js
+++ test-docker/conf/jasmine.conf.js
@@ -62,7 +62,7 @@ module.exports = function (config) {
         junitReporter: {
             
             outputDir: 'target/test_reports/junit',
-            outputFile: 'target/test_reports/junit/junit.xml/', // if included, results will be saved as $outputDir/$browserName/$outputFile
+            outputFile: 'junit.xml/', // if included, results will be saved as $outputDir/$browserName/$outputFile
             suite: '', // suite will become the package name attribute in xml testsuite element
             useBrowserName: false, // add browser name to report and classes names
             nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
